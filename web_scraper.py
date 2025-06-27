4abd474fe1e0fc212006aedc38bde1166774ce31
NareshAiNexus <nare95143@gmail.com>
2025-06-27 10:27:13 +0530

    Program fixed

--- web_scraper.py
+++ web_scraper.py
@@ -3,32 +3,37 @@ from bs4 import BeautifulSoup
 
 
 def scrape(url):
-    # for i in range(len(urls)):
 
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    # print(soup)
-    with open("soup1.html", "w") as f:
+    
+    return soup
+
+def soup2HTML(url, path, filename):
+    soup = scrape(url)
+    path = path + filename
+    # create the file if not exist and write the scrapped code or text
+    with open(path, "w") as f:
         f.write(str(soup))
-        # selected_content = tag_extractions(soup)
 
-    # title = soup.select_one('h1').text
-    # text = soup.select_one('p').text
-    # link = soup.select_one('a').get('href')
 
-def tag_extractions(soup):
-    basic_tags = set('h1', 'h2', 'h3', 'h3', 'h4', 'h5', 'h6',
-                  'p', 'a', 'span', 'strong', 'b', 'img')
+def soup2MD(url, path, filename):
     # just the basic text and images we need to extract from the url 
     # still our main objective is to extract the content.
+    soup = scrape(url)
+    
+    MD_map = {'h1':"#", 'h2':'##', 'h3':'###', 'h4':'####', 'h5':'#####',
+              'a': '[', 'p':'', 'i':'*', 'b':'**'}
 
-    content = ""
-    for tag in soup:
-        if tag in basic_tags:
-            pass
-        pass
+    
 
 if __name__ == "__main__":
-    url = "https://www.example.com"
-    scrape(url)
+    url = input("Enter the URL : ")
+    path = input("Enter the path : ")
+    file_name = input("Enter the filename : ")
+    try:
+        soup2HTML(url, path, file_name)
+    except Exception as e:
+        print("ERROR OCCURED")
+        
 
