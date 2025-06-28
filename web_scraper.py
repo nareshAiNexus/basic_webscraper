946deab4653c1b082e8e29ea46f2c65481de0376
NareshAiNexus <nare95143@gmail.com>
2025-06-28 09:56:33 +0530

    Soup2MD created

--- web_scraper.py
+++ web_scraper.py
@@ -17,15 +17,47 @@ def soup2HTML(url, path, filename):
         f.write(str(soup))
 
 
-def soup2MD(url, path, filename):
-    # just the basic text and images we need to extract from the url 
-    # still our main objective is to extract the content.
+   def soup2MD(url, path, filename):
     soup = scrape(url)
-    
-    MD_map = {'h1':"#", 'h2':'##', 'h3':'###', 'h4':'####', 'h5':'#####',
-              'a': '[', 'p':'', 'i':'*', 'b':'**'}
+    path = path + filename
 
-    
+    md_lines = []
+
+    for tag in soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'b', 'i', 'a', 'img']):
+        if tag.name in ['h1', 'h2', 'h3', 'h4', 'h5']:
+            md_lines.append(f"{'#' * int(tag.name[1])} {tag.get_text(strip=True)}\n")
+        
+        elif tag.name == 'p':
+            text = tag.get_text(strip=True)
+            if text:
+                md_lines.append(f"{text}\n")
+
+        elif tag.name == 'b':
+            text = tag.get_text(strip=True)
+            if text:
+                md_lines.append(f"**{text}**\n")
+
+        elif tag.name == 'i':
+            text = tag.get_text(strip=True)
+            if text:
+                md_lines.append(f"*{text}*\n")
+
+        elif tag.name == 'a':
+            href = tag.get('href', '')
+            text = tag.get_text(strip=True)
+            if href and text:
+                md_lines.append(f"[{text}]({href})\n")
+
+        elif tag.name == 'img':
+            src = tag.get('src', '')
+            alt = tag.get('alt', 'Image')
+            if src:
+                md_lines.append(f"![{alt}]({src})\n")
+
+    # Write the Markdown lines to a file
+    with open(path, "w", encoding="utf-8") as f:
+        f.write("\n".join(md_lines))
+
 
 if __name__ == "__main__":
     url = input("Enter the URL : ")
